7f8acbe20b846acdf3bddf4794b3cf631d1a90aa
yaw owusu <asirifiyaw@gmail.com>
2017-12-21 23:08:33 -0800

    created a scorecard

--- src/App.js
+++ src/App.js
@@ -5,27 +5,34 @@ import cards from "./cards.json";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import GameCard from "./components/GameCard";
+import Score from "./components/Score";
 
 class App extends Component {
   constructor(){
     super();
     this.state={
-      cards: cards
+      cards: cards,
+      score: 0
     }
   }
 
+increment = () => {
+  this.setState({score: this.state.score + 1})
+}
+
   render() {
+      
     return (
       <Wrapper>
         <Title>Clicky Game 
           <p>Click on an image to earn points, but don't click on any more than once!</p>
-          
+          <p>
+            <h3>Score: {this.state.score}</h3>
+          </p>
         </Title>
-          {this.state.cards.map(card => (
-          <GameCard image={card.image} />        
-        ))}
-      </Wrapper>
       
+          {this.state.cards.map(card => (<GameCard image={card.image} />))} 
+      </Wrapper>
     );
   }
 }
---
+++ src/components/Score.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+var Score = 0;
+
+const currentScore = () => (
+<div className = "main">
+	<div className = "container">
+	</div>
+</div>
+
+
+)
+
+
+export default Score;
